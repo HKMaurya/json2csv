043f98670106669173ee7dc8b9502b882f80fe2b
Hemant Maurya <hemant.maurya@harappa.education>
2023-01-19 16:47:35 +0530

    update

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-const converter = require('json-2-csv')
-const fs = require('fs')
+const express = require('express');
+const converter = require('json-2-csv');
+const fs = require('fs');
+const app = express();
 // const legacy = JSON.parse(fs.readFileSync('./LegacyReport.json'));
 // const legacyIsDemo = JSON.parse(fs.readFileSync('./LegacyReport-isDemo.json'));
 // const learnerProgress = JSON.parse(fs.readFileSync('./LearnerProgressReport.json'));
@@ -7,16 +9,23 @@ const fs = require('fs')
 // const b2cIos = JSON.parse(fs.readFileSync('./b2c-ios-all.json'));
 // const b2cIosOrder = JSON.parse(fs.readFileSync('./b2c-Ios-OrderReports.json'));
 // const LearnerActivationLegavy = JSON.parse(fs.readFileSync('./LearnerActivationLegavy.json'));
-const B2C = JSON.parse(fs.readFileSync('./B2C.json'));
+// const B2C = JSON.parse(fs.readFileSync('./B2C.json'));
 
-// convert JSON array to CSV string
-converter.json2csv(B2C, (err, csv) => {
-  if (err) {
-    throw err
-  }
+// // convert JSON array to CSV string
+// converter.json2csv(B2C, (err, csv) => {
+//   if (err) {
+//     throw err
+//   }
 
-  // print CSV string
-  console.log(csv)
-  // write CSV to a file
-  fs.writeFileSync('B2C_data.csv', csv)
+//   // print CSV string
+//   console.log(csv)
+//   // write CSV to a file
+//   fs.writeFileSync('B2C_data.csv', csv)
+// })
+app.get('*', (req, res) => {
+  res.send('Hello World')
+});
+
+app.listen(5050, () => {
+  console.log('Server is running on 5050')
 })
